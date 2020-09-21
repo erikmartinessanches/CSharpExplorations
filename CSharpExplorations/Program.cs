7bc02f006d8f813ccd922a2d3bc403b53aaab9e9
Erik Martines Sanches <erik.martinessanches@gmail.com>
2020-09-21 11:13:27 +0200

    Starting a null example.

--- CSharpExplorations/Program.cs
+++ CSharpExplorations/Program.cs
@@ -4,8 +4,23 @@ namespace CSharpExplorations
 {
     public class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            NullExamples();
+        }
 
-
+        private static void NullExamples()
+        {
+            int cantBeNull = 5;
+            //cantBeNull = null; //error, clearly
+            int? couldBeNull = null;
+            Console.WriteLine(couldBeNull); //Writes nothing
+            Console.WriteLine(couldBeNull.GetValueOrDefault()); //0
+            couldBeNull = 7;
+            Console.WriteLine(couldBeNull); //7
+            Console.WriteLine(couldBeNull.GetValueOrDefault()); //7
+            //couldBeNull = null;
+            int? y = couldBeNull;
+        }
     }
 }
