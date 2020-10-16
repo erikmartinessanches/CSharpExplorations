d34d404a795cec8d49d32e847b11facbf7c03e27
Erik Martines Sanches <erik.martinessanches@gmail.com>
2020-10-16 19:01:13 +0200

    * Octopus.cs: * CSharpExplorations.csproj:
    * Program.cs: Constructors, deconstructors and expression-bodied
      properties.

---
+++ CSharpExplorations/Octopus.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace CSharpExplorations
+{
+    public class Octopus
+    {
+        int Legs;
+        int Weight;
+        int length; /* The private backing field for the Length proprety 
+                     * (Not necessary for automatic properties). */
+
+        /* Expression-bodied prop. "value" is implicitly set to the assigned 
+         * value at the calling site. */
+        public int Length { get => length - 3; set => length = value + 5; }
+        
+
+        /* Constructor overloading, using "this" keyword to call another
+         * constructor from a constructor. */
+        public Octopus() //Explicitly write default one (if desired) when others exist.
+        {
+
+        }
+        public Octopus(int legs) { Legs = legs; }
+        // Below, the called constructor executes first.
+        public Octopus(int legs, int weight) : this(legs) { Weight = weight; }
+
+
+        public void Eat(int fishes)
+        {
+            Console.WriteLine($"Ate {fishes} fishes.");
+        }
+
+        public void Deconstruct(out int legs, out int weight)
+        {
+            legs = Legs;
+            weight = Weight;
+        }
+    }
+}
--- CSharpExplorations/Program.cs
+++ CSharpExplorations/Program.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿
+using System;
 
 namespace CSharpExplorations
 {
+    /// <summary>
+    /// This program is for trying out some select aspects of C#, since I 
+    /// already know some OOP.
+    /// </summary>
     public class Program
     {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args)
+        {
+            ConstructingAndDeconstructing();
+        }
 
+        private static void ConstructingAndDeconstructing()
+        {
+            Octopus octo = new Octopus(8, 35);
+            octo.Eat(4);
+            var (legs, weight) = octo; //Deconstructing, equivalent to:
+            //octo.Deconstruct(out var legs, out var weight);
+            Console.WriteLine($"Legs: {legs}, weight: {weight} units.");
+            octo.Eat(6);
 
+            //Using properties with extra set and get logic.
+            octo.Length = 50; 
+            Console.WriteLine(octo.Length);
+        }
     }
 }
